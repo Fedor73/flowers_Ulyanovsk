342793d1d2f52d73088e121446c18e4749775524
FedorVinecky <31040475+Fedor73@users.noreply.github.com>
2019-03-22 23:27:16 +0400

    add arrow section

--- js/internal.js
+++ js/internal.js
@@ -3,16 +3,16 @@ if ($('.js-image-wrapper').length > 0) {
 }
 
 function toggleClassMenu() {
-	myMenu.classList.add("menu--animatable");	
-	if(!myMenu.classList.contains("menu--visible")) {		
-		myMenu.classList.add("menu--visible");
-	} else {
-		myMenu.classList.remove('menu--visible');		
-	}	
+    myMenu.classList.add("menu--animatable");
+    if (!myMenu.classList.contains("menu--visible")) {
+        myMenu.classList.add("menu--visible");
+    } else {
+        myMenu.classList.remove('menu--visible');
+    }
 }
 
 function OnTransitionEnd() {
-	myMenu.classList.remove("menu--animatable");
+    myMenu.classList.remove("menu--animatable");
 }
 
 var myMenu = document.querySelector(".menu");
@@ -20,3 +20,19 @@ var oppMenu = document.querySelector(".menu-icon");
 myMenu.addEventListener("transitionend", OnTransitionEnd, false);
 oppMenu.addEventListener("click", toggleClassMenu, false);
 myMenu.addEventListener("click", toggleClassMenu, false);
+
+
+
+$(document).ready(function () {
+    $("a[href]").on("click", function (e) {
+        var myMenu = document.querySelector(".menu");
+        myMenu.classList.remove('menu--visible');
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+
+    });
+});
